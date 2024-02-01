f8de900cd3fb5dcefafc3102da7f1f44dfeaacbe
Antonio <jantonio6.java@gmail.com>
2024-02-01 14:19:52 -0500

    primer avance, pagina index, nosotros y contacto, se agrego menu y estilos a todas la paginas

---
+++ assets/js/contacto.js
@@ -0,0 +1,45 @@
+//validar correo
+function validarCorreo(correo){
+    var expReg = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    var esValido = expReg.test(correo);
+    if(esValido == true){
+        console.log("es valido el correo");
+    }
+    else{
+        alert("el correo no es válido");
+    }
+}
+
+//validar nombres
+function sololetras(e){
+    key = e.keyCode || e.which;
+    teclado = String.fromCharCode(key).toLowerCase();
+    letras = " abcdefghijklmnñopqrstuvwxyzáéíóúü";
+    especiales = "8-37-38-46-164";
+    teclado_especial = false;
+    for(var i in especiales){
+        if(key == especiales[i]){
+            teclado_especial = true; break;
+        }
+    }
+    if(letras.indexOf(teclado) == -1 && !teclado_especial){
+        return false;
+    }
+}
+
+//validar numeros
+function solonumeros(e){
+    key = e.keyCode || e.which;
+    teclado = String.fromCharCode(key).toLowerCase();
+    numeros = "0123456789";
+    especiales = "8-37-38-46-164";
+    teclado_especial = false;
+    for(var i in especiales){
+        if(key == especiales[i]){
+            teclado_especial = true; break;
+        }
+    }
+    if(numeros.indexOf(teclado) == -1 && !teclado_especial){
+        return false;
+    }
+}
